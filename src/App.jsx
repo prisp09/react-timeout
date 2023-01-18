df24b4fcc73247499104493730cabf0f22ffb919
Priyanshu Patel <97778509+prisp09@users.noreply.github.com>
2023-01-18 12:42:23 -0500

    Timeout added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,52 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
+const maxTime = 1000 * 15; // 15 minutes  
+const userActivity = [
+  "click",
+  "scroll",
+  "keypress",
+  "mousemove",
+  "mousedown",
+  "mousewheel",
+];
+
 const App = () => {
-  const [timer, setTimer] = React.useState(9000);
+  let timer;
+  const [timerCount, setTimerCount] = useState(maxTime);
+
+  const updateCount = () => {
+    timer = !timer && setInterval(() => {
+      setTimerCount((prevCount) => prevCount - 1000);
+    }, 1000);
+
+    if (timerCount === 0) {
+      clearInterval(timer);
+    }
+  };
+
+  useEffect(() => {
+    updateCount();
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [timerCount]);
 
   return (
     <div className="App">
       <h1>Timeout example</h1>
+      
       <br />
-      <h2>Time until timeout: {timer/1000} seconds</h2>
+      <br />
+     
+      {userActivity.forEach((input) => {
+        document.addEventListener(input, () => {
+        timerCount > 0 ? setTimerCount(maxTime) : setTimerCount(0);
+      })})
+      }
+      {timerCount === 0 ? <h2>Timeout!</h2> : <h2>Time until timeout: {(timerCount / (1000))} seconds</h2>}  
+      
     </div>
   );
 };
